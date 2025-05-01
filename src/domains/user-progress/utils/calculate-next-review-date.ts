5cb411b4326a331dea281028dfaf9c7a23161fa5
Pavel Borisov <pavellbor@gmail.com>
2025-05-01 20:10:14 +0300

    refactor: изменить время возврата даты следующего обзора на 1 минуту

--- src/domains/user-progress/utils/calculate-next-review-date.ts
+++ src/domains/user-progress/utils/calculate-next-review-date.ts
@@ -27,7 +27,7 @@ export function calculateNextReviewDate(
   const now = dayjs();
 
   if (!isCorrect) {
-    return now.toDate();
+    return now.add(1, 'minute').toDate();
   }
 
   // Для правильных ответов используем интервалы из массива,
